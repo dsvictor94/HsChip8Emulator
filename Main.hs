3d5387892b8a9ceb811a65238d6b77e3f9e9cf64
Victor Duarte da Silva <ds.victor94@gmail.com>
2014-09-21 05:15:47 -0300

    Drawing sprite bug fixed

--- Main.hs
+++ Main.hs
@@ -1,9 +1,10 @@
 import Data.Word
-import Data.Array
+import Data.Array.Unboxed
 import Prelude hiding (foldl, readFile)
 import Data.Bits
 import Data.ByteString (readFile, unpack)
 
+import System.Environment
 import Debug.Trace
 import Numeric
 
@@ -34,15 +35,39 @@ toKeyboard 11 = CKey
 toKeyboard 15 = VKey            
 
 
-data Chip8 = Chip8 { memory::Array Word16 Word8,
-                     graphics::Array Int Bool,
-                     register::Array Word8 Word8,
+data Chip8 = Chip8 { memory::UArray Word16 Word8,
+                     graphics::UArray Int Bool,
+                     register::UArray Word8 Word8,
                      indexRegister::Word16,
                      programCounter::Word16,
                      stack::[Word16],
                      delayTimer::Word8
                    } deriving Show
 
+                   
+modifyMemory::Word16 -> (Word8 -> Word8) -> Chip8 -> Chip8
+modifyMemory pox f c = c {memory = memory c // [(pox, f $ memory c ! pox)]}
+
+modifyPixel::Int -> Int -> (Bool -> Bool) -> Chip8 -> Chip8
+modifyPixel x y f c = c {graphics = graphics c // [(pox, f $ graphics c ! pox)]}
+  where 
+    pox = y*64 + x
+                   
+modifyRegister::Word8 -> (Word8 -> Word8) -> Chip8 -> Chip8
+modifyRegister x f c = c {register = register c // [(x, f $ register c ! x)]}
+
+modifyIndex::(Word16 -> Word16) -> Chip8 -> Chip8
+modifyIndex f c = c {indexRegister = f $ indexRegister c}
+
+modifyPC::(Word16 -> Word16) -> Chip8 -> Chip8
+modifyPC f c = c {programCounter = f $ programCounter c}
+
+pushStack::Chip8 -> Chip8
+pushStack c = c { stack = (programCounter c):stack c}
+
+popStack::Chip8 -> (Word16 ,Chip8)
+popStack c = (head $ stack c,c { stack = tail $ stack c})
+                   
 newArray (a, b) v = array (a, b) $ [(i, v) | i<-enumFromTo a b]                    
 
 chip8Fontset = [0xF0, 0x90, 0x90, 0x90, 0xF0, -- 0
@@ -63,7 +88,7 @@ chip8Fontset = [0xF0, 0x90, 0x90, 0x90, 0xF0, -- 0
                 0xF0, 0x80, 0xF0, 0x80, 0x80  -- F
                ]
 
-defaultState = Chip8 { memory = newArray (0, 4095) 0 // zip [0..] chip8Fontset,
+defaultState = Chip8 { memory = newArray (0, 4095) 0 // (zip [0..] chip8Fontset),
                        graphics = newArray (0, 2047) False,
                        register = newArray (0, 15) 0,
                        indexRegister = 0,
@@ -90,97 +115,76 @@ toNN (n1:n2:[]) = fromWord4 n1 n2
 
 
 
-c_00EE c = c {
-  programCounter = (head $ stack c) + 2, 
-  stack = tail $ stack c
-}
+c_00EE c = let (adr, chip8) = popStack c
+           in modifyPC (const $ adr+2) chip8
 
-c_00E0 c = c {
-  graphics = newArray (0, 2047) False,
-  programCounter = programCounter c + 2
-}
+c_00E0 c = modifyPC (+2) $ c {graphics = newArray (0, 2047) False}
 
-c_1NNN adr c = c { programCounter = adr}
+c_1NNN adr = modifyPC (const adr)
 
-c_2NNN adr c = c {
-  stack  =  (programCounter c : stack c),
-  programCounter = adr
-}
+c_2NNN adr = (modifyPC $ const adr).pushStack
 
 c_3XNN x n c = let vx = register c ! x
-               in c { programCounter = programCounter c + if vx == n then 4 else 2 }
+               in modifyPC (+ if vx == n then 4 else 2) c
 
 c_4XNN x n c = let vx = register c ! x
-               in c { programCounter = programCounter c + if vx /= n then 4 else 2 }
+               in modifyPC (+ if vx /= n then 4 else 2) c
 
 c_5XY0 x y c = let vx = register c ! x
                    vy = register c ! y
-               in c { programCounter = programCounter c + if vx == vy then 4 else 2 }               
+               in modifyPC (+ if vx == vy then 4 else 2) c
                
-c_6XNN x n c = c {
-    register = register c // [(x,n)],
-    programCounter = programCounter c + 2
-  }
+c_6XNN x n = (modifyPC (+2)).modifyRegister x (const n) 
 
-c_7XNN x n c = let vx = register c ! x
-               in c { register = register c // [(x, vx+n)],
-                      programCounter = programCounter c + 2 }
+c_7XNN x n = (modifyPC (+2)).modifyRegister x (+n) 
                       
 c_8XY0 x y c = let vy = register c ! y
-               in c { register = register c // [(x, vy)],
-                      programCounter = programCounter c + 2 }  
+               in (modifyPC (+2)).modifyRegister x (const vy) $ c
 
-c_8XY1 x y c = let vx = register c ! x
-                   vy = register c ! y
-               in c { register = register c // [(x, vx .|. vy)],
-                      programCounter = programCounter c + 2 }                        
+c_8XY1 x y c = let vy = register c ! y
+               in (modifyPC (+2)).modifyRegister x (.|. vy) $ c
 
-c_8XY2 x y c = let vx = register c ! x
-                   vy = register c ! y
-               in c { register = register c // [(x, vx .&. vy)],
-                      programCounter = programCounter c + 2 } 
+c_8XY2 x y c = let vy = register c ! y
+               in (modifyPC (+2)).modifyRegister x (.&. vy) $ c
                       
-c_8XY3 x y c = let vx = register c ! x
-                   vy = register c ! y
-               in c { register = register c // [(x, vx `xor` vy)],
-                      programCounter = programCounter c + 2 }                                              
+c_8XY3 x y c = let vy = register c ! y
+               in (modifyPC (+2)).modifyRegister x (`xor` vy) $ c                        
                       
 c_8XY4 x y c = let vx = register c ! x
                    vy = register c ! y
-               in c { register = register c // [(x, vx+vy), (15, if vy <= 0xff-vx then 0 else 1)],
-                      programCounter = programCounter c + 2 }  
+                   carry = if vy <= 0xff-vx then 0 else 1
+               in (modifyPC (+2)).(modifyRegister x (+vy)).(modifyRegister 15 $ const carry) $ c
                       
 c_8XY5 x y c = let vx = register c ! x
                    vy = register c ! y
-               in c { register = register c // [(x, vx-vy), (15, if vx < vy then 0 else 1)],
-                      programCounter = programCounter c + 2 }  
+                   borrow = if vx < vy then 0 else 1
+               in (modifyPC (+2)).(modifyRegister x (subtract vy)).(modifyRegister 15 $ const borrow) $ c  
 
 c_8XY6 x y c = let vy = register c ! y
-               in c { register = register c // [(x, vy `shiftR` 1), (15, vy `shiftL` 7)],
-                      programCounter = programCounter c + 2 }  
+               in (modifyPC (+2)).(modifyRegister x $ const $ vy `shiftR` 1).(modifyRegister 15 $ const $ vy `shiftL` 7) $ c
 
 c_8XY7 x y c = let vx = register c ! x
                    vy = register c ! y
-               in c { register = register c // [(x, vy-vx), (15, if vy < vx then 0 else 1)],
-                      programCounter = programCounter c + 2 } 
+                   borrow = if vy > vx then 1 else 0
+               in (modifyPC (+2)).(modifyRegister x (vy-)).(modifyRegister 15 $ const borrow) $ c  
 
 c_8XYE x y c = let vy = register c ! y
-               in c { register = register c // [(x, vy `shiftL` 1), (15, vy `shiftR` 7)],
-                      programCounter = programCounter c + 2 }                        
+               in (modifyPC (+2)).(modifyRegister x $ const $ vy `shiftL` 1).(modifyRegister 15 $ const $ vy `shiftR` 7) $ c
 
 c_9XY0 x y c = let vx = register c ! x
                    vy = register c ! y
-               in c { programCounter = programCounter c + if vx /= vy then 4 else 2 }                      
+               in modifyPC (const $ if vx /= vy then 4 else 2) c                     
 
-c_ANNN n c = c { indexRegister = n, programCounter = programCounter c + 2 }
+c_ANNN n = (modifyPC (+2)).modifyIndex (const n)
 
-c_BNNN n c = c { programCounter = n + (fromIntegral $ register c ! 0) }
+c_BNNN n c = let offset = fromIntegral $ register c ! 0
+             in modifyPC (const $ n+offset) c
 
-c_CXNN ran x n c = c { register = register c // [(x, ran .&. n)], programCounter = programCounter c + 2 }
+c_CXNN ran x n = (modifyPC (+2)).modifyRegister x (const $ ran .&. n)
              
 c_DXYN rx ry n c = c { 
-    graphics = gfx // spriteMap (\x y ->  (pixel x y, gfx!(pixel x y) /= bit x y)),
-    register = register c // [(15, if or (spriteMap (\x y -> (gfx!pixel x y) && (bit x y))) then 1 else 0)],
+    graphics = gfx // spriteMap (\x y ->  (pixel x y, not $ gfx!(pixel x y))),
+    register = register c // [(15, if or (spriteMap (\x y -> (gfx!pixel x y))) then 1 else 0)],
     programCounter = programCounter c + 2
   }
   where
@@ -189,45 +193,50 @@ c_DXYN rx ry n c = c {
     vy = register c ! ry
     i = indexRegister c
     pixel x y = (fromIntegral (vy) + fromIntegral (y))*64 + fromIntegral (vx) + fromIntegral (x)
-    bit x y = (memory c ! i+y) .&. (0x80 `shiftR` x) /= 0 
-    spriteMap f = [ f x y | y <- [0..(n-1)], x <- [0..7]]
+    bit x y = ((memory c ! (i+fromIntegral y)) .&. (0x80 `shiftR` x)) /= 0 
+    spriteMap f = [ f x y | y <- [0..(n-1)], x <- [0..7], bit x y]
     
 c_EX9E inp x c = let vx = register c ! x
-                 in c { programCounter = programCounter c + if toKeyboard vx `elem` inp then 4 else 2 }    
+                 in modifyPC (+ if toKeyboard vx `elem` inp then 4 else 2) c
 
 c_EXA1 inp x c = let vx = register c ! x
-                 in c { programCounter = programCounter c + if not $ toKeyboard vx `elem` inp then 4 else 2 }                     
+                 in modifyPC (+ if not $ toKeyboard vx `elem` inp then 4 else 2) c
                  
-c_FX07 x c = c { register = register c // [(x, delayTimer c)], programCounter = programCounter c + 2 }                     
+c_FX07 x c = let delay = delayTimer c 
+             in (modifyPC (+2)).modifyRegister x (const delay) $ c
 
+c_FX0A inp x = case someKey of
+                    Nothing -> id
+                    Just k -> (modifyPC (+2)).modifyRegister x (const k)
+  where 
+    someKey = case [ x | x <- [0..15], toKeyboard x `elem` inp] of
+                   []   -> Nothing
+                   x:[] -> Just x
+                      
+             
 c_FX15 x c = let vx = register c ! x
-             in c { delayTimer = vx, programCounter = programCounter c + 2 }
+             in modifyPC (+2) $ c { delayTimer = vx}
 
 c_FX1E x c = let vx = register c ! x
-                 i = indexRegister c
-             in c { indexRegister = i + fromIntegral vx, programCounter = programCounter c + 2 }
+             in modifyPC (+2) $ modifyIndex (+ fromIntegral vx) $ c
 
 c_FX29 x c = let vx = register c ! x
-             in c { indexRegister = (fromIntegral x) *5, programCounter = programCounter c + 2 }
+             in  modifyPC (+2) $ modifyIndex (const $ fromIntegral vx*5) c
 
 c_FX33 x c = let vx = register c ! x
                  i = indexRegister c
                  d3 = vx `rem` 10
-                 d2 = d3 `rem` 10
-                 d1 = d2 `rem` 10
-             in c { memory = memory c //[(i, d1),(i+1, d2), (i+2, d3)] , programCounter = programCounter c + 2 }
+                 d2 = (vx `div` 10) `rem` 10
+                 d1 = vx `div` 100
+             in modifyPC (+2) $ c { memory = memory c //[(i, d1),(i+1, d2), (i+2, d3)]}
 
 c_FX55 x c = let v = (!) (register c)
                  i = indexRegister c
-             in c { indexRegister = i+(fromIntegral (v x))+1,
-                    memory = memory c // [(i + (fromIntegral n), fromIntegral (v n)) | n <- [0..x]],
-                    programCounter = programCounter c + 2 }             
-
+             in modifyPC (+2) $ c { memory = memory c // [(i + (fromIntegral n), v n) | n <- [0..x]]}
+             
 c_FX65 x c = let i = indexRegister c
-                 mem n = (!) (memory c) $ (+) i $ fromIntegral n
-             in c { indexRegister = i+(fromIntegral (register c ! x))+1,
-                    register = register c // [(n, mem n) | n <- [0..x]],
-                    programCounter = programCounter c + 2 }                    
+                 mem n = memory c ! (fromIntegral n + i)
+             in modifyPC (+2) $ c {register = register c // [(n, mem n) | n <- [0..x]]}                    
                     
 niy::String -> Chip8 -> Chip8
 niy str c = trace str c {programCounter = (programCounter c) + 2}
@@ -251,8 +260,10 @@ showGraphics chip8 = concat $ map line $ fst $ part ([[]], elems g)
     
 process::(Time, [Key], Word8) -> Chip8 -> Chip8
 process (t, inp, ran) chip8 =
-  trace (showGraphics chip8 ++ "\n\n") $  
+  --trace (showGraphics chip8 ++ "\n\n") $  
   --trace ("PC: "++(showHex pc "") ++" OPCODE: "++ concat (map (\x -> showHex x "") opcode) ++ " STACK: "++ (show $ stack chip8))
+  --trace ("\n" ++ concat (map (\x -> printf "%02x" x) (elems $ memory chip8)) )
+  --trace ("R: " ++ (show $ elems $ register chip8))
   (case opcode of
         0x0:0x0:0xe:0xe:[] -> c_00EE
         0x0:0x0:0xe:0x0:[] -> c_00E0
@@ -281,6 +292,7 @@ process (t, inp, ran) chip8 =
         0xe:x  :0x9:0xe:[] -> c_EX9E inp x
         0xe:x  :0xa:0x1:[] -> c_EXA1 inp x
         0xf:x  :0x0:0x7:[] -> c_FX07 x
+        0xf:x  :0x0:0xa:[] -> c_FX0A inp x
         0xf:x  :0x1:0x5:[] -> c_FX15 x
         0xf:x  :0x1:0x8:[] -> niy "FX18"
         0xf:x  :0x1:0xe:[] -> c_FX1E x
@@ -288,7 +300,7 @@ process (t, inp, ran) chip8 =
         0xf:x  :0x3:0x3:[] -> c_FX33 x
         0xf:x  :0x5:0x5:[] -> c_FX55 x
         0xf:x  :0x6:0x5:[] -> c_FX65 x
-        _                  -> niy "!!!!") $ updateTimers chip8
+        _                  -> niy ("["++show opcode ++ "]")) $ updateTimers chip8
   where 
     pc = programCounter chip8
     mem = memory chip8
@@ -299,7 +311,7 @@ input = lift3 (, ,) delta keysDown random
 
 render :: Chip8 -> (Int, Int) -> Element
 render chip8 (w, h) =
-    collage w h screen
+  collage w h screen
   where
     half = (/ 2). fromIntegral
     size = ((fromIntegral w /64)-2, (fromIntegral h /32)-2)
@@ -315,7 +327,7 @@ loadFile path = do
 
 main :: IO ()
 main = do
-    program <- loadFile "PONG2"
+    program <- fmap head getArgs >>= loadFile
     engine <- startup config
 
     run engine $ render <~ (stepper program) ~~ Window.dimensions engine
